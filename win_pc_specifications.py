e37c93718cae823cdde8e4225b57ca92e0e26d27
mazhar004 <princemazhar.mp@gmail.com>
2022-02-07 14:51:18 +0600

    Add linux support & upgrade the functions

--- win_pc_specifications.py
+++ win_pc_specifications.py
@@ -1,37 +1,39 @@
 import os
-from datetime import datetime
+import sys
+import re
 import json
+from datetime import datetime
+from collections import defaultdict
+
 
 global date_index
 date_index = [[365, 'year'], [30, 'month'], [1, 'day']]
 
 
-def process_data(val):
-    """Process cmd prompt data"""
-    all_data = {}
-    val = val.strip().split('\n')
-    key = ""
-    for item in val:
-        sub_item = item.split(': ')
-        if len(sub_item) == 2:
-            sub_item1, sub_item2 = sub_item[0].strip(
-            ).lower(), sub_item[1].strip().lower()
-            if sub_item1:
-                key = sub_item1
-                all_data[key] = []
-            all_data[key].append(sub_item2)
-    return all_data
+def convert_date(val, d_format="%m/%d/%Y", r_format="%d %b, %Y"):
+    # Convert date string to datetime object or vice versa
+    try:
+        date_time_obj = datetime.strptime(val, d_format)
+    except:
+        val = re.search(r'[\d]{1,}/[\d]{1,}/[\d]{4}', val).group()
+        date_time_obj = datetime.strptime(val, d_format)
+    date_time_str = date_time_obj.strftime(r_format)
+    return date_time_obj, date_time_str
 
 
-def get_command_data():
-    """cmd propmt data get & process pipeline"""
-    command = "systeminfo.exe"
-    val = os.popen(command).read()
-    return process_data(val)
+def process_data(cmd_data):
+    # Process the command data
+    all_lines = cmd_data.splitlines()
+    data = defaultdict(list)
+    for line in all_lines:
+        if ':' in line:
+            key, val = map(str.strip, line.split(':', 1))
+            data[key.lower()].append(val)
+    return data
 
 
 def date_format(val, index=0, str_val=''):
-    """Date time format to build datetime object"""
+    # Convert dat time object to string
     if index < 3:
         divisor, name = date_index[index]
         quotient = val//divisor
@@ -42,45 +44,57 @@ def date_format(val, index=0, str_val=''):
 
 
 def age_calculate(date_time_obj):
-    """Calculate age"""
+    # Calculate the age
     age = datetime.now()-date_time_obj
     return date_format(age.days)
 
 
-def format_data(all_data):
-    """Build JSON data"""
-    json_data = {}
-    json_data['model_name'] = all_data['system model'][-1]
-    json_data['brand_name'] = all_data['system manufacturer'][-1]
+def format_data(data, keys):
+    # Format the data based on keys
+    dict_data = {}
+    for key, vals in keys.items():
+        dict_data[vals[0]] = data[key][0]
+        if len(vals) == 2:
+            date_time_obj, dict_data[vals[0]] = convert_date(
+                dict_data[vals[0]])
+            dict_data[vals[1]] = age_calculate(date_time_obj)
 
-    os_install_date = all_data['original install date'][0]
-    json_data['os_install_date'] = datetime.strptime(
-        os_install_date, "%m/%d/%Y, %I:%M:%S %p")
-    json_data['os_age'] = age_calculate(json_data['os_install_date'])
-    json_data['os_install_date'] = json_data['os_install_date'].strftime(
-        "%a, %d %b %Y")
-
-    manufacture_date = all_data['bios version'][-1].split(',')[-1].strip()
-    json_data['manufacture_date'] = datetime.strptime(
-        manufacture_date, "%m/%d/%Y")
-    json_data['device_age'] = age_calculate(json_data['manufacture_date'])
-    json_data['manufacture_date'] = json_data['manufacture_date'].strftime(
-        "%a, %d %b %Y")
-    return json_data
+    return dict_data
 
 
 def get_command_data():
-    """Run cmd for system info"""
-    command = "systeminfo.exe"
-    val = os.popen(command).read()
-    return process_data(val)
+    # Get the command data based on different OS
+    platform = (sys.platform).lower()
+    command = ''
+    if 'win' in platform:
+        command = "systeminfo.exe"
+        keys = {'system model': ['Model Name'],
+                'system manufacturer': ['Brand Name'],
+                'original install date': ['OS Install Date', 'OS Age'],
+                'bios version': ['Manufacture Date', 'Device Age']}
+
+    elif platform == 'linux':
+        command = "dmidecode"
+        keys = {'release date': ['Manufacture Date', 'Device Age'],
+                'manufacturer': ['Brand Name'],
+                'product name': ['Model Number'],
+                'family': ['Model Family'],
+                'maximum capacity': ['RAM Size']}
+    cmd_data = os.popen(command).read()
+    return cmd_data, keys
+
+
+def show(json_data, save=False):
+    # Show the data & save based on save flag
+    str_json_data = json.dumps(json_data, indent=2)
+    print(str_json_data)
+    if save:
+        with open("pc_specification.txt", "w") as fh:
+            fh.write(str_json_data)
 
 
 if __name__ == "__main__":
-    """Full pipeline for system specification"""
-    all_data = get_command_data()
-    json_data = format_data(all_data)
-    str_json_data = json.dumps(json_data, indent=2)
-    print(str_json_data)
-    with open("pc_specification.txt", "w") as fh:
-        fh.write(str_json_data)
+    cmd_data, keys = get_command_data()
+    data = process_data(cmd_data)
+    json_data = format_data(data, keys)
+    show(json_data, save=True)
